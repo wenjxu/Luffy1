14277dcd4a06feeef7976c8594aedbea851579d6
wenjxu <1060321320@qq.com>
2019-03-07 11:01:58 +0800

    加个exception_wrapper装饰器

--- api/views/course.py
+++ api/views/course.py
@@ -6,17 +6,15 @@ class CourseView(viewsets.ModelViewSet):
     queryset = models.Course.objects.all()
     serializer_class = CourseSerializer
     res = {'code': '1000', 'data': None}
-    # 这个方法没有添加装饰器
-    # 测试解决bug
+
+    # 为这个方法添加装饰器
+    @exception_wrapper
     def retrieve(self, request, *args, **kwargs):
-        try:
-            pk =kwargs.get('pk')
-            course_detail_obj = models.CourseDetail.objects.filter(pk=pk).first()
-            cs = CourseDetailSerializer(course_detail_obj)
-            self.res['data'] =cs.data
-        except:
-            traceback.print_exc()
-            self.res['code'] ='1001'
+
+        pk =kwargs.get('pk')
+        course_detail_obj = models.CourseDetail.objects.filter(pk=pk).first()
+        cs = CourseDetailSerializer(course_detail_obj)
+        self.res['data'] =cs.data
         return Response(data=self.res)
         # 函数 = 装饰器(函数)
 
